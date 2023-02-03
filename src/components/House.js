a98363a309dc89553c5bb200b6ee4b33758dc8c3
kmhill242 <kmhill242@gmail.com>
2023-02-03 14:27:42 -0500

    first

---
+++ src/components/House.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { NewRoomForm } from "./NewRoomForm";
+
+export const House = (props) => {
+  const { house, updateHouse } = props;
+
+  const deleteRoom = (roomId) => {
+    const updatedHouse = {
+      ...house,
+      rooms: house.rooms.filter((x) => x._id !== roomId),
+    };
+    updateHouse(updatedHouse);
+  };
+  //using new array to take rooms from old array and add a new room to that array
+  const addNewRoom = (room) =>
+    updateHouse({ ...house, rooms: [...house.rooms, room] });
+
+  const rooms = () => (
+    <ul>
+      {house.rooms.map((room, index) => (
+        <li key={index}>
+          <label> {`${room.name} Area: ${room.area}`}</label>
+          <button onClick={(e) => deleteRoom(room._id)}>Delete</button>
+        </li>
+      ))}
+    </ul>
+  );
+
+  return (
+    <div>
+      <h1>{house.name}</h1>
+      {rooms({ rooms, houseId: house._id, deleteRoom })}
+      <NewRoomForm addNewRoom={addNewRoom} />
+    </div>
+  );
+};
